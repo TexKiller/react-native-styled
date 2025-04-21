b8bf748d235410763f2774a025b8bc02e95aa39f
Faister Cabrera Carvalho <faister.carvalho@toptal.com>
2025-04-21 06:20:40 -0300

    fix: revert className detection change

--- src/components/WebSelectors/index.tsx
+++ src/components/WebSelectors/index.tsx
@@ -13,12 +13,7 @@ const WebSelectors = ({
   const ref = React.useRef<typeof OriginalComponent>(null);
   const [hash, setHash] = React.useState("");
   React.useEffect(() => {
-    setHash(
-      (ref.current as any).className
-        .split(" ")
-        .find((c: string) => c.startsWith("css-")) ||
-        (ref.current as any).className,
-    );
+    setHash((ref.current as any).className.split(" ").pop());
   }, [setHash]);
   (props as any).ref = ref;
   return (
