7af2741a287c97d70e1928b00e5ad0af057bf158
Faister Cabrera Carvalho <faister.carvalho@toptal.com>
2025-04-26 21:00:40 -0300

    fix: text style inheritance on native

--- src/utils/styles.ts
+++ src/utils/styles.ts
@@ -1,19 +1,9 @@
 export const fixFontStyle = (style: any) => {
   if (!style) {
-    return {
-      fontWeight: 400,
-      color: "inherit",
-      textDecoration: "inherit",
-      whiteSpace: "inherit",
-      overflowWrap: "inherit",
-    };
+    return { fontWeight: 400 };
   }
   let weight = 400;
   let family = "";
-  let color = "inherit";
-  let textDecoration = "inherit";
-  let whiteSpace = "inherit";
-  let overflowWrap = "inherit";
   const styles = [...(style instanceof Array ? style : [style])];
   for (const style of styles) {
     if (style?.fontWeight) {
@@ -22,18 +12,6 @@ export const fixFontStyle = (style: any) => {
     if (style?.fontFamily) {
       family = style.fontFamily;
     }
-    if (style?.color) {
-      color = style.color;
-    }
-    if (style?.textDecoration) {
-      textDecoration = style.textDecoration;
-    }
-    if (style?.whiteSpace) {
-      whiteSpace = style.whiteSpace;
-    }
-    if (style?.overflowWrap) {
-      overflowWrap = style.overflowWrap;
-    }
   }
   if ((weight as any) === "normal") {
     weight = 400;
@@ -41,10 +19,6 @@ export const fixFontStyle = (style: any) => {
     weight = 700;
   }
   styles[styles.length - 1].fontWeight = weight;
-  styles[styles.length - 1].color = color;
-  styles[styles.length - 1].textDecoration = textDecoration;
-  styles[styles.length - 1].whiteSpace = whiteSpace;
-  styles[styles.length - 1].overflowWrap = overflowWrap;
   if (family && !family.includes("-")) {
     styles[styles.length - 1].fontFamily =
       (weight < 500 && `${family}-Regular`) ||
